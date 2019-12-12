7c6b113f09eacff34496cfd1321602d7a21e94eb
Atanas <tumbalev10@gmail.com>
2019-12-12 15:13:39 +0200

    Added Merge Sort

--- Excercices and Implementations/4. Sorting Algorithms/MergeSort.cpp
+++ Excercices and Implementations/4. Sorting Algorithms/MergeSort.cpp
@@ -1,74 +1,73 @@
 #include <iostream>
 
+
 // Merges two subarrays of arr[]. 
 // First subarray is arr[l..m] 
 // Second subarray is arr[m+1..r] 
-void merge(int arr[], int l, int m, int r) 
-{ 
-	int n1 = m - l + 1; 
-	int n2 = r - m; 
+template <typename T>
+void merge(T arr[], unsigned beg, unsigned mid, unsigned end) 
+{
+	unsigned leftArrSize = mid - beg + 1;
+	unsigned rightArrSize = end - mid;
 
-	/* create temp arrays */
-	int L[n1], R[n2]; 
-
-	/* Copy data to temp arrays L[] and R[] */
-	for (int i = 0; i < n1; i++) 
+	/* Init subarrays*/
+	T left[leftArrSize];
+	T right[rightArrSize];
+	/* Copy elements from original arr into subarrays */
+	for (int i = 0; i < leftArrSize; ++i)
 	{
-		L[i] = arr[l + i]; 
-	}
-	
-	for (int j = 0; j < n2; j++) 
-	{
-		R[j] = arr[m + 1+ j]; 
+		left[i] = arr[beg + i];
 	}
 
-	/* Merge the temp arrays back into arr[l..r]*/
-	int i = 0; // Initial index of first subarray 
-	int j = 0; // Initial index of second subarray 
-	int k = l; // Initial index of merged subarray 
-	while (i < n1 && j < n2) 
-	{ 
-		if (L[i] <= R[j]) 
-		{ 
-			arr[k] = L[i]; 
-			i++; 
-		} 
+	for (int i = 0; i < rightArrSize; ++i)
+	{
+		right[i] = arr[mid + i + 1];
+	}
+
+
+	unsigned i = 0; // @left iteration index
+	unsigned j = 0; // @right iteration index
+	unsigned k = beg; // @arr iteration index
+
+	while ( i < leftArrSize && j < rightArrSize)
+	{
+		if (left[i] <= right[j])
+		{
+			arr[k] = left[i];
+			++i;
+		}
 		else
-		{ 
-			arr[k] = R[j]; 
-			j++; 
-		} 
-		k++; 
-	} 
+		{
+			arr[k] = right[j];
+			++j;
+		}
+		++k;
+	}
 
-	/* Copy the remaining elements of L[], if there 
-	are any */
-	while (i < n1) 
-	{ 
-		arr[k] = L[i]; 
-		i++; 
-		k++; 
-	} 
+	/* Dump remaining elements of remaining array */
+	while (i < leftArrSize)
+	{
+		arr[k] = left[i];
+		++i;
+		++k;
+	}
 
-	/* Copy the remaining elements of R[], if there 
-	are any */
-	while (j < n2) 
-	{ 
-		arr[k] = R[j]; 
-		j++; 
-		k++; 
-	} 
+	while (j < rightArrSize)
+	{
+		arr[k] = right[j];
+		++j;
+		++k;
+	}
 } 
 
 /* l is for left index and r is right index of the 
 sub-array of arr to be sorted */
-void mergeSort(int arr[], int l, int r) 
-{ 
+template <typename T>
+void mergeSort(T arr[], int l, int r) 
+{
 	if (l < r) 
-	{ 
-		// Same as (l+r)/2, but avoids overflow for 
-		// large l and h 
-		int m = l+(r-l)/2; 
+	{
+		int m = (r + l) / 2; 
 
 		// Sort first and second halves 
 		mergeSort(arr, l, m); 
@@ -80,7 +79,8 @@ void mergeSort(int arr[], int l, int r)
 
 /* UTILITY FUNCTIONS */
 /* Function to print an array */
-void printArray(int A[], int size) 
+template <typename T>
+void printArray(T A[], int size) 
 { 
 	for (int i = 0; i < size; i++) 
 	{
@@ -90,7 +90,6 @@ void printArray(int A[], int size)
 	std::cout << std::endl;
 } 
 
-/* Driver program to test above functions */
 int main() 
 { 
 	int arr[] = {12, 11, 13, 5, 6, 7}; 
