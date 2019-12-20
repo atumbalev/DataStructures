e704a53848a5ba002a41a50109a290427ef1e0d6
Atanas <tumbalev10@gmail.com>
2019-12-20 18:24:46 +0200

    Added all ways to place N Queens on NxN board

--- Bonus Tasks/8Queens.cpp
+++ Bonus Tasks/8Queens.cpp
@@ -1,8 +1,12 @@
+// In how many ways can we place N queens on an NxN chessboard
+// without havin 2 queens threaten each other?
+// Example solution with size = 8
 #include <iostream>
 #include <set>
 
 using std::set;
 
+// size of the board, also equal to the number of queens
 #define size 8
 
 bool board[size][size];
@@ -11,14 +15,15 @@ set<int> attackedColumns;
 set<int> attackedLeftDiagonals;
 set<int> attackedRightDiagonals;
 
+// counter that displays how many different variations we found
 unsigned variations = 0;
 
 bool canPlaceQueen(int row, int col)
 {
 	bool positionTaken =
-			attackedColumns.count(col) ||
-			attackedLeftDiagonals.count(col - row) ||
-			attackedRightDiagonals.count(row + col);
+			attackedColumns.count(col) ||				// Check if column is attacked
+			attackedLeftDiagonals.count(col - row) ||	// All squares in the same left diagonal have the same col - row
+			attackedRightDiagonals.count(row + col);	// All squares in the same right diagonal have the same row + col
 
 	return !positionTaken;
 }
@@ -41,7 +46,6 @@ void unmarkFields(int row, int col)
 
 void printSolution()
 {
-	variations++;
 	for (int row = 0; row < size; row++)
 	{
 		for (int col = 0; col < size; col++)
@@ -54,19 +58,24 @@ void printSolution()
 	std::cout << '\n';
 }
 
+// Recursively finds a solution for the given row
 void solve(int row)
 {
+	// we have placed a queen on every row, so a solution is found
 	if (row == size) 
 	{
+		variations++;
 		printSolution();
 		return;
 	}
 
+	// We iterate over every column of that row, trying to place a 
 	for (int col = 0; col < size; ++col)
 	{
 		if (canPlaceQueen(row, col))
-		{
+		{	// we mark and unmark so we can explore different variations later on
 			markAttackedFields(row, col);
+			// we dive into 1 current variation
 			solve(row +1);
 			unmarkFields(row,col);
 		}
